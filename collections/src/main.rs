3dea8c97c4458f3bd6e42c415ecbf116c5882a63
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-02-23 20:21:00 +0530

    Hashmaps

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut v: Vec<i32> = Vec::new();
 
@@ -18,5 +20,27 @@ fn main() {
     //loop
     for i in v {
         println!("Looping {}", i);
+    }
+
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    let array = [
+        0, 1, 3, 4, 5, 6, 7, 8, 9, 5, 4, 3, 2, 3, 3, 4, 3, 23, 2, 4, 42, 24, 4, 2, 2, 3, 2, 3,
+    ];
+
+    for n in array {
+        let count = map.entry(n).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", &map);
+
+    for (k, v) in map {
+        println!(
+            "the number {} is occured {} {}.",
+            k,
+            v,
+            if v == 1 { "time" } else { "times" }
+        );
     }
 }
